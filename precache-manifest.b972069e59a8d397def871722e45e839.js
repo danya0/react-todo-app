c754619b0f7f6a0e91c073adf46e500ff8140b64
unknown <you@example.com>
2020-08-27 10:07:08 +0300

    Updates

---
+++ precache-manifest.b972069e59a8d397def871722e45e839.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ef06e7a018b0bc07735d753867f31a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "828e367e05ce9d157331",
+    "url": "/react-todo-app/static/css/main.ba4e8380.chunk.css"
+  },
+  {
+    "revision": "f0b823749dd01d98426b",
+    "url": "/react-todo-app/static/js/2.22ec8ef8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.22ec8ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828e367e05ce9d157331",
+    "url": "/react-todo-app/static/js/main.b6f26969.chunk.js"
+  },
+  {
+    "revision": "c05100418fe52825fd71",
+    "url": "/react-todo-app/static/js/runtime-main.b74b7ac8.js"
+  },
+  {
+    "revision": "a6dfbed73934e95ab6f08feabed4da00",
+    "url": "/react-todo-app/static/media/arrow.a6dfbed7.svg"
+  },
+  {
+    "revision": "3a5afd7357c0a2c43162c69e03cf7d5a",
+    "url": "/react-todo-app/static/media/check.3a5afd73.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/react-todo-app/static/media/close.cad70081.svg"
+  }
+]);
